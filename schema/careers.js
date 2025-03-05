54b011a0deb264397098945ffcb1d070600e50d2
Haylie Tan <haylie.tan.code@gmail.com>
2025-03-05 10:47:56 -0800

    Careers

---
+++ schema/careers.js
@@ -0,0 +1,28 @@
+const { ContentType, FieldType } = require('@include/hearth');
+
+const programCards = new ContentType({
+  name: 'careers',
+  pluralDisplayName: 'Get Involved: Careers',
+});
+
+programCards
+  .createField({
+    name: 'position',
+    displayName: 'Position',
+    type: FieldType.SHORT_TEXT,
+    required: true,
+  })
+  .createField({
+    name: 'description',
+    displayName: 'Description',
+    type: FieldType.LONG_TEXT,
+    required: true,
+  })
+  .createField({
+    name: 'link',
+    displayName: 'Link to Job',
+    type: FieldType.LONG_TEXT,
+    required: true,
+  });
+
+module.exports = programCards;
