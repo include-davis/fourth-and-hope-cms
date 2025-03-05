02950ee1254a123c239726ab644c89a704da48d6
Haylie Tan <haylie.tan.code@gmail.com>
2025-03-05 10:38:02 -0800

    About: Meetings Pop-up

---
+++ schema/meetings.js
@@ -0,0 +1,28 @@
+const { ContentType, FieldType } = require('@include/hearth');
+
+const programCards = new ContentType({
+  name: 'meetings',
+  pluralDisplayName: 'About: Board Meetings Pop-up',
+});
+
+programCards
+  .createField({
+    name: 'year',
+    displayName: 'Year',
+    type: FieldType.SHORT_TEXT,
+    required: true,
+  })
+  .createField({
+    name: 'month',
+    displayName: 'Month',
+    type: FieldType.SHORT_TEXT,
+    required: true,
+  })
+  .createField({
+    name: 'link',
+    displayName: 'PDF Link',
+    type: FieldType.LONG_TEXT,
+    required: true,
+  });
+
+module.exports = programCards;
