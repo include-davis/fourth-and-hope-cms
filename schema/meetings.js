f53333f5202f1daf17d37beb5c6a7a6cfbb38f5d
Haylie Tan <haylie.tan.code@gmail.com>
2025-03-27 17:48:32 -0700

    Updated meetings

--- schema/meetings.js
+++ schema/meetings.js
@@ -5,24 +5,97 @@ const meetings = new ContentType({
   pluralDisplayName: 'About: Board Meetings Pop-up',
 });
 
-meetings
-  .createField({
-    name: 'year',
-    displayName: 'Year',
-    type: FieldType.SHORT_TEXT,
-    required: true,
-  })
-  .createField({
-    name: 'month',
-    displayName: 'Month',
-    type: FieldType.SHORT_TEXT,
-    required: true,
-  })
-  .createField({
-    name: 'link',
-    displayName: 'PDF Link',
-    type: FieldType.SHORT_TEXT,
-    required: true,
-  });
+// Required field for the year
+meetings.createField({
+  name: 'year',
+  displayName: 'Year',
+  type: FieldType.SHORT_TEXT,
+  required: true,
+});
+
+// Optional media-list fields for each month (for PDF links)
+meetings.createField({
+  name: 'january',
+  displayName: 'January PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'february',
+  displayName: 'February PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'march',
+  displayName: 'March PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'april',
+  displayName: 'April PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'may',
+  displayName: 'May PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'june',
+  displayName: 'June PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'july',
+  displayName: 'July PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'august',
+  displayName: 'August PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'september',
+  displayName: 'September PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'october',
+  displayName: 'October PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'november',
+  displayName: 'November PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
+
+meetings.createField({
+  name: 'december',
+  displayName: 'December PDF',
+  type: FieldType.MEDIA_LIST,
+  required: false,
+});
 
 module.exports = meetings;
