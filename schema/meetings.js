28d5d022fb53b1f664534f7c7a2731e280f2aec2
Haylie Tan <haylie.tan.code@gmail.com>
2025-03-27 21:25:05 -0700

    Updated media_list to short_text on meetings

--- schema/meetings.js
+++ schema/meetings.js
@@ -17,84 +17,84 @@ meetings.createField({
 meetings.createField({
   name: 'january',
   displayName: 'January PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'february',
   displayName: 'February PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'march',
   displayName: 'March PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'april',
   displayName: 'April PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'may',
   displayName: 'May PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'june',
   displayName: 'June PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'july',
   displayName: 'July PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'august',
   displayName: 'August PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'september',
   displayName: 'September PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'october',
   displayName: 'October PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'november',
   displayName: 'November PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
 meetings.createField({
   name: 'december',
   displayName: 'December PDF',
-  type: FieldType.MEDIA_LIST,
+  type: FieldType.SHORT_TEXT,
   required: false,
 });
 
